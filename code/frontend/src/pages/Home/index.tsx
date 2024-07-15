65c8870c61eefd74993d48e1bf2241826a54bca3
MathieuPlateforme <mathieu.ruiz@laplateforme.io>
2024-07-15 14:16:25 +0200

    feedcard

--- code/frontend/src/pages/Home/index.tsx
+++ code/frontend/src/pages/Home/index.tsx
@@ -1,22 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../../components/ExploreContainer';
-import './index.css';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import FeedCard from '../../components/FeedCard';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>TailwindCSS</IonTitle>
+          <IonTitle>Spotty</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen className="relative">
-        <div className="text-center absolute left-0 right-0 top-1/2 transform -translate-y-1/2 flex justify-center items-center">
-          <div className="border border-gray-100 rounded-xl shadow-2xl p-8 bg-gradient-to-r from-green-400 via-blue-900 to-blue-400 text-white max-w-lg">
-            <h1 className="text-2xl font-bold">Using TailwindCSS in Ionic with React</h1>
-            <p className="mt-4">This is an example of how you can use <span className="font-bold">TailwindCSS</span> in an <span className="font-bold">Ionic</span> application using <span className="font-bold">React</span> framework.</p>
-          </div>
-        </div>
+        <FeedCard
+          username="John Doe"
+          timestamp="2021-08-12 12:00"
+          title="Hello World"
+          description="This is a test description"
+          imageUrl="https://via.placeholder.com/800x400"
+          avatarUrl="https://via.placeholder.com/50"
+        />
+        <IonButton color="primary">Primary</IonButton>
       </IonContent>
     </IonPage>
   );
