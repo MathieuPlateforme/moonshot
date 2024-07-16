26b25f3d63e31c2af68b54d2c59ac2809457d390
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2024-07-16 14:58:15 +0200

    feat(user-service): initialize basic API with Actix-Web
    - Implement the base API with only the /health route using Actix-Web.
    - Add '.env' file for environment variables with examples and explanations for each variable.
    - Update 'Cargo.lock' for dependency locking.
    - Add 'log' and 'env_logger' dependencies in 'Cargo.toml' for logging.
    - Create 'src/config.rs' for application configuration, including server address and logging initialization.
    - Add 'src/handlers.rs' with only a 'health_check' handler for now.
    - Update 'src/main.rs' to start the Actix-Web API.
    - Set up 'src/routes' module for routing, currently only includes '/health' route.

--- code/backend/user-service/src/main.rs
+++ code/backend/user-service/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+use std::io::Result;
+
+mod config;
+mod routes;
+mod handlers;
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    config::init_logging();
+    let server_address = config::server_address();
+
+    HttpServer::new(|| {
+        App::new()
+            .configure(routes::configure)
+    })
+    .bind(server_address)?
+    .run()
+    .await
 }
