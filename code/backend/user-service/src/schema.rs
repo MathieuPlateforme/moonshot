fc6f3a971549a81d0e8d20347c63b36cd530cc5e
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2024-07-17 15:31:31 +0200

    feat(user-service): initialize Diesel setup with models and schema
    - Add initial Diesel migration for setup
    - Create models for Users and Address
    - Generate schema from Diesel migration
    - Update Cargo.toml with necessary dependencies for Diesel, Chrono, and UUID

---
+++ code/backend/user-service/src/schema.rs
@@ -0,0 +1,37 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    addresses (address_id) {
+        address_id -> Uuid,
+        user_id -> Uuid,
+        street -> Varchar,
+        city -> Varchar,
+        state -> Varchar,
+        postal_code -> Varchar,
+        country -> Varchar,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    users (user_id) {
+        user_id -> Uuid,
+        firstname -> Varchar,
+        lastname -> Varchar,
+        email -> Varchar,
+        birthdate -> Date,
+        username -> Varchar,
+        phone -> Nullable<Varchar>,
+        role -> Varchar,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
+
+diesel::joinable!(addresses -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    addresses,
+    users,
+);
