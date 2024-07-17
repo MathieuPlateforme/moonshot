5540294fb924bbb2be2c9de874bb4a62b78ec261
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2024-07-17 15:03:24 +0200

    refactor(user-service/migrations): change id columns to UUID in create_users and create_addresses

--- code/backend/user-service/migrations/2024-07-16-172916_create_users/up.sql
+++ code/backend/user-service/migrations/2024-07-16-172916_create_users/up.sql
@@ -1,6 +1,7 @@
--- Your SQL goes here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     firstname VARCHAR NOT NULL,
     lastname VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
--- code/backend/user-service/migrations/2024-07-16-172923_create_addresses/up.sql
+++ code/backend/user-service/migrations/2024-07-16-172923_create_addresses/up.sql
@@ -1,12 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE addresses (
-address_id SERIAL PRIMARY KEY,
-user_id INTEGER NOT NULL,
-street VARCHAR NOT NULL,
-city VARCHAR NOT NULL,
-state VARCHAR NOT NULL,
-postal_code VARCHAR NOT NULL,
-country VARCHAR NOT NULL,
-created_at TIMESTAMP NOT NULL,
-updated_at TIMESTAMP NOT NULL,
-FOREIGN KEY (user_id) REFERENCES users (user_id)
+    address_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL,
+    street VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    state VARCHAR NOT NULL,
+    postal_code VARCHAR NOT NULL,
+    country VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
