052b2aac88e3e5ba3b497c0537ad3f5ab1d0ca12
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2024-07-25 02:47:54 +0200

    maj

--- code/frontend/src/pages/Event/components/EventCard.tsx
+++ code/frontend/src/pages/Event/components/EventCard.tsx
@@ -22,14 +22,16 @@ interface EventCardProps {
 
 const EventCard: React.FC<EventCardProps> = ({ event, onButtonClick }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden max-w-md mt-5" onClick={onButtonClick}>
-      <img className="w-full" src={`data:image/jpeg;base64,${event?.media.file}`} alt="Event" />
-      <div className="p-4">
-        <p className="text-purple-600 mb-1">{event?.subEvents[0]?.start_date}</p>
-        <h2 className="text-xl font-bold mb-2 mt-0 text-black">{event?.title}</h2>
-        <p className="text-purple-600 self-end">{event?.description}</p>
-        <p className="text-grey-600 self-end">{event?.subEvents[0]?.address}</p>
-        <p className="text-green-600">{event?.total_participants} participants</p>
+    <div className="p-4">
+      <div className="rounded-lg shadow-green overflow-hidden max-w-md mt-5" onClick={onButtonClick}>
+        <img className="p-2 rounded-2xl" src={`data:image/jpeg;base64,${event?.media.file}`} alt="Event" />
+        <div className="p-4">
+          <p className="card-date mb-1">{event?.subEvents[0]?.start_date}</p>
+          <h2 className="card-title mb-2 mt-0 text-black">{event?.title}</h2>
+          <p className="card-description p-2 self-end">{event?.description}</p>
+          <p className="card-adress self-end">{event?.subEvents[0]?.address}</p>
+          <p className="font-be-vietnam text-14 font-regular text-green">{event?.total_participants} participants</p>
+        </div>
       </div>
     </div>
   );
